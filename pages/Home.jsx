3fc833803d43d646dc27299250fde39271f4c528
evyatar shlar <evyatar.szlar@gmail.com>
2025-01-23 20:04:20 +0200

    Edit homepage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,6 +1,10 @@
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home!</h1>
+    return <section className="home flex align-center justify-center">
+        <div>
+        <h1>Secure, smart, and easy to use email & keep</h1>
+        <h5>Get more done with our mail. Now integrated with our keep, and more, all in one place.</h5>
+        </div>
+        <img src="./assets/img/homepage.jpg"/>
     </section>
 }
