8388c7e5c58eb03cda5d3618161f5456f5fb0e0f
Shmuel shif <shmulikshif@gmail.com>
2025-01-23 21:09:28 +0200

    Change home and about

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,11 +1,53 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+
+        <div className="my-img">
+            <img 
+                src="../assets/img/WhatsApp Image 2024-08-15 at 18.48.06.jpeg" 
+                alt="Header icon" 
+                className="my-profile"
+            />
+
+            <img 
+                src="../assets/img/WhatsApp Image 2025-01-23 at 20.09.05.jpeg" 
+                alt="Header icon" 
+                className="my-profile"
+            />
+        </div>
+        <p >
+            Welcome to our website, created by Evyatar and Shmuel using React! <br></br>
+            This platform is designed to help you easily manage and organize your <br></br>
+            emails and notes in one seamless place.
+        </p>
+
         <p>
-        Welcome to our website, created by Evyatar and Shmuel using React! This platform is designed to help you easily manage and organize your emails and notes in one seamless place.
-        </p><p> With a simple, intuitive interface, we’ve built a user-friendly experience that allows you to quickly access, send, and organize your messages and notes with ease.
-        </p><p> Whether you're working on personal projects or collaborating with others,
-        </p><p>  our app is here to make your daily communication and organization more efficient. We hope you enjoy using the tool as much as we enjoyed creating it!</p>
+            With a simple, intuitive interface, we’ve built a user-friendly <br></br>
+            experience that allows you to quickly access, send,<br></br>
+            and organize your messages and notes with ease.
+        </p>
+
+        <p>
+            Whether you're working on personal projects or collaborating with others.
+        </p>
+
+        <p>
+            our app is here to make your daily communication and organization more efficient.<br></br>
+            We hope you enjoy using the tool as much as we enjoyed creating it!
+        </p>
+
+        <div className="logo-img">
+            <img 
+                src="../assets/img/keep-512.png" 
+                alt="Header icon" 
+                className="keep-mail"
+            />
+
+            <img 
+                src="../assets/img/Gmail_icon_(2020).svg.webp"
+                alt="Header icon" 
+                className="keep-mail"
+            />
+        </div>
     </section>
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,12 @@
 
 export function Home() {
-    return <section className="home flex align-center justify-center">
+    return <section className="home">
         <div>
-        <h1>Secure, smart, and easy to use email & keep</h1>
-        <h5>Get more done with our mail. Now integrated with our keep, and more, all in one place.</h5>
+        <h1>Secure, smart, and <br></br>
+         easy to use email & keep
+         </h1>
+        <h6>Get more done with our mail. Now integrated with our keep,<br></br>
+            and more, all in one place.</h6>
         </div>
         <img src="./assets/img/homepage.jpg"/>
     </section>
