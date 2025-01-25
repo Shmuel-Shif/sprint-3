920e2493cd077388f4b11b5ff0c89418dcfb8968
evyatar shlar <evyatar.szlar@gmail.com>
2025-01-26 00:25:42 +0200

    Add font awsom

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -1,8 +1,9 @@
 const { useState, useEffect } = React
 
-export function MailFolderList({unReadCount ,starredCount , draftCount, filterBy, onSetFilter}){
+export function MailFolderList({ unReadCount, starredCount, draftCount, filterBy, onSetFilter }) {
 
     const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
+    // const [readType, setReadType] = useState('all')
 
     useEffect(() => {
         onSetFilter(filterByToEdit)
@@ -11,7 +12,9 @@ export function MailFolderList({unReadCount ,starredCount , draftCount, filterBy
     function handleChange({ target }) {
         // target.classList.add("selected")
         const { name: field, value, type } = target
+        console.log(value, field)
         setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
+        // if (field === 'isRead')  setReadType(value)
     }
 
     // function onSortBy({ target }) {
@@ -19,11 +22,36 @@ export function MailFolderList({unReadCount ,starredCount , draftCount, filterBy
     //     onSetFilter(filterByToEdit)
     // }
 
-    return <ul className='mail-folder flex column clean-list'>
+    // function handleChangeRead({ target }) {
+    //     const { name: field, value, type } = target
+
+    //     setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
+    // }
+    
+    return <section><ul className='mail-folder flex column clean-list'>
         <li onClick={() => handleChange({ target: { name: 'status', value: 'inbox' } })} className='selected'>Inbox   <span>   {(unReadCount) ? unReadCount : ''}</span></li>
-        <li onClick={() => handleChange({ target: { name: 'status', value: 'starred'}})}>Starred<span>   {(starredCount) ? starredCount : ''}</span></li>
+        <li onClick={() => handleChange({ target: { name: 'status', value: 'starred' } })}>Starred<span>   {(starredCount) ? starredCount : ''}</span></li>
         <li onClick={() => handleChange({ target: { name: 'status', value: 'sent' } })}>Sent</li>
         <li onClick={() => handleChange({ target: { name: 'status', value: 'draft' } })}>Draft <span>   {(draftCount) ? draftCount : ''}</span></li>
         <li onClick={() => handleChange({ target: { name: 'status', value: 'trash' } })}>Trash</li>
     </ul>
+        {/* <article>
+            <div>
+                <input onChange={handleChange} type="radio" id="all" name="isRead" value={''} checked={readType === 'all'} />
+                <label htmlFor="all">All</label>
+            </div>
+
+            <div>
+                <input onChange={handleChange} type="radio" id="read" name="isRead" value={true} checked={readType === 'read'} />
+                <label htmlFor="read">Read</label>
+            </div>
+
+            <div>
+                <input onChange={handleChange} type="radio" id="unread" name="isRead" value={false} checked={readType === 'unread'} />
+                <label htmlFor="unread">Unread</label>
+            </div>
+        </article> */}
+    </section>
+
+
 }
