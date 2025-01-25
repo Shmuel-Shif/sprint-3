ea012d5ec0e2406566ec7f253eaaaf2d903f9aa2
Shmuel shif <shmulikshif@gmail.com>
2025-01-25 21:48:41 +0200

    His blood

--- apps/note/cmps/NoteVideo.jsx
+++ apps/note/cmps/NoteVideo.jsx
@@ -1,10 +1,16 @@
-export function NoteVideo({ url }) {
+export function NoteVideo({ videoUrl }) {
+    // const embedUrl = videoUrl.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
+
     return (
         <div className="note-video">
-            <video width="100%" controls>
-                <source src={url} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <iframe 
+                width="100%" 
+                height="220" 
+                // src={embedUrl} 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen>
+            </iframe>
         </div>
-    )
+    );
 }
