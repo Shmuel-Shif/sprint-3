7584444ddd168b4a6dc0bcd2a6038771f357c5b5
evyatar shlar <evyatar.szlar@gmail.com>
2025-01-25 22:03:05 +0200

    Edit responsiv

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -25,7 +25,7 @@ export function MailPreview({ mail, onSelectMail, onRemove, onReadMail,onStarred
         onMouseOver={() => setIsOver(true)}
         onMouseOut={() => setIsOver(false)} >
         <span className='star' onClick={onSetStarredMail}>{(mail.isStarred) ? '⭐' : '✡️'}</span>
-        <span className='mail-preview-from'>{mail.from}</span>
+        <span className='mail-preview-from'>{(mail.from === 'user@appsus.com') ? 'Me': mail.from}</span>
         <div className='mail-preview-subject'>
             <p><span className={(mail.isRead) ? '' : 'bold'}>{mail.subject}</span><span className='gray'> - {mail.body}</span></p>
         </div>
@@ -33,5 +33,6 @@ export function MailPreview({ mail, onSelectMail, onRemove, onReadMail,onStarred
         {isOver && <div className='buttens flex align-center space-between' >
             <button className='btn' onClick={onSetRemove}>🗑️</button>
             <button className='btn' onClick={onSetReadMail}>📧</button></div>}
+            <span className='sender-logo'>{mail.from.charAt(0)}</span>
     </article>
 }
